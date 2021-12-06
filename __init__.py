fba7311e30578401f5d2aa0ed11a87f51b716eb3
samolajide23 <d00231093@student.dkit.ie>
2021-12-06 14:14:26 +0000

    Push

--- __init__.py
+++ __init__.py
@@ -1,9 +1,11 @@
+import flask
+import flask_mysqldb
 from flask import Flask, render_template, request, session, redirect, url_for
 from flask_mysqldb import MySQL, MySQLdb
 
 app = Flask(__name__)
 app.secret_key = "AzurCam123"
-#yX6dCdPZF9ggdQ2kHPGA
+# yX6dCdPZF9ggdQ2kHPGA
 app.config["MYSQL_HOST"] = "localhost"
 app.config["MYSQL_USER"] = "root"
 app.config["MYSQL_PASSWORD"] = ""
@@ -18,14 +20,16 @@ def index():
         if 'email' in request.form and 'password' in request.form:
             email = request.form['email']
             password = request.form['password']
+
             cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
             cursor.execute("SELECT * FROM users WHERE email=%s AND password =%s", (email, password))
             info = cursor.fetchone()
             print(info)
+
             if info is not None:
                 if info['email'] == email and info['password'] == password:
                     session['loginsuccess'] = True
-                    return redirect(url_for('profile'))
+                    return redirect(url_for('home'))
             else:
                 print("Failed Login")
                 return redirect(url_for('index'))
@@ -41,14 +45,18 @@ def account():
 @app.route('/new', methods=['GET', 'POST'])
 def registration():
     if request.method == "POST":
-        if "one" in request.form and "two" in request.form and "three" in request.form:
-            username = request.form['one']
-            email = request.form['two']
-            password = request.form['three']
-            cur = db.connection.cursor(MySQLdb.DictCursor)
-            cur.execute("INSERT INTO azurcam.users(username, email, password)VALUES(%s, %s, %s)",
-                        (username, email, password))
-            db.connection.comit()
+        if "name" in request.form and "username" in request.form and "email" in request.form and "password" in request.form:
+            name = request.form['name']
+            username = request.form['username']
+            email = request.form['email']
+            password = request.form['password']
+
+            cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute("INSERT INTO azurcam.users(name, username, email, password)VALUES(%s, %s, %s, %s)",
+                           (name, username, email, password))
+
+            db.connection.commit()
+            print("2")
             return redirect(url_for('index'))
     return render_template("registration.html")
 
@@ -61,8 +69,12 @@ def profile():
 
 @app.route('/home')
 def home():
-    #  if session['loginsuccess'] == True:
     return render_template("home.html")
+
+
+@app.route('/help')
+def helper():
+    return render_template('help.html')
 
 
 @app.route('/new/logout')
