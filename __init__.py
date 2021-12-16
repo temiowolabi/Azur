07a2716868f05e2c77dc03bea4105348394bdc72
samolajide23 <d00231093@student.dkit.ie>
2021-12-16 10:58:35 +0000

    Sends Images to AWS Storage

--- __init__.py
+++ __init__.py
@@ -1,16 +1,21 @@
-from functools import wraps
-
 import flask
 import flask_mysqldb
-from flask import Flask, render_template, request, session, redirect, url_for, flash
+import pubnub
+import uuid
+from flask import Flask, render_template, request, session, redirect, url_for
 from flask_mysqldb import MySQL, MySQLdb
+
+from pubnub.callbacks import SubscribeCallback
+from pubnub.pnconfiguration import PNConfiguration
+from pubnub.pubnub import PubNub
+from pprint import pprint
 
 app = Flask(__name__)
 app.secret_key = "AzurCam123"
 # yX6dCdPZF9ggdQ2kHPGA
 app.config["MYSQL_HOST"] = "localhost"
 app.config["MYSQL_USER"] = "root"
-app.config["MYSQL_PASSWORD"] = "yX6dCdPZF9ggdQ2kHPGA"
+app.config["MYSQL_PASSWORD"] = ""
 app.config["MYSQL_DB"] = "azurcam"
 
 db = MySQL(app)
@@ -18,6 +23,7 @@ db = MySQL(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    session['loginsuccess'] = False
     if request.method == 'POST':
         if 'email' in request.form and 'password' in request.form:
             email = request.form['email']
@@ -26,11 +32,16 @@ def index():
             cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
             cursor.execute("SELECT * FROM users WHERE email=%s AND password =%s", (email, password))
             info = cursor.fetchone()
-            print(info)
 
             if info is not None:
                 if info['email'] == email and info['password'] == password:
+                    #save variables
                     session['loginsuccess'] = True
+                    session['user_id'] = info['user_id']
+                    session['name'] = info['name']
+                    session['username'] = info['username']
+                    session['email'] = info['email']
+                    session['iot_id'] = info['iot_id']
                     return redirect(url_for('home'))
             else:
                 print("Failed Login")
@@ -39,7 +50,13 @@ def index():
     return render_template("index.html")
 
 
-
+@app.route("/account")
+def account():
+    if session['loginsuccess']:
+        return render_template("account.html")
+    else:
+        (print("Not Signed In"))
+        return render_template("index.html")
 
 
 @app.route('/new', methods=['GET', 'POST'])
@@ -56,47 +73,65 @@ def registration():
                            (name, username, email, password))
 
             db.connection.commit()
-            print("2")
             return redirect(url_for('index'))
     return render_template("registration.html")
 
-def LoginRequired(f):
-    @wraps(f)
-    def wrapper(*args, **kwargs):
-        if "logged_in" in session:
-            if session['logged_in']:
-                return f(*args, **kwargs)
-            flash("Please login first")
-            return redirect(url_for("login"))
-    return wrapper
-
-# @app.route('/new/profile')
-# def profile():
-#     #  if session['loginsuccess'] == True:
-#     return render_template("profile.html")
-
 
 @app.route('/home')
-# @LoginRequired
 def home():
-    return render_template("home.html")
+    if session['loginsuccess']:
+        iot_id = session['iot_id']
+        cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
+        sql = "SELECT * FROM images WHERE image_reference LIKE '%s%%' " % iot_id
+        cursor.execute(sql)
+        info = cursor.fetchall()
+        array = []
+        for ino in info:
+            array.append(ino["image_reference"])
+        return render_template("home.html", array=array)
+    else:
+        (print("Not Signed In"))
+        return render_template("index.html")
 
-@app.route("/account")
-# @LoginRequired
-def account():
-    return render_template("account.html")
 
 @app.route('/help')
-# @LoginRequired
 def helper():
-    return render_template('help.html')
+    if session['loginsuccess']:
+        return render_template('help.html')
+    else:
+        (print("Not Signed In"))
+        return render_template("index.html")
 
 
 @app.route('/new/logout')
 def logout():
-    session.pop('loginsuccess', None)
+    session['loginsuccess'] = False
     return redirect(url_for('index'))
 
+
+"""
+RECIEVING DETECTED MOTION FROM PUBNUB
+"""
+
+
+class MySubscribeCallback(SubscribeCallback):
+    def message(self, pubnub, message):
+        messageArray = message.__dict__
+        print(messageArray['message']['sender'])
+        cursor = db.cursor()
+        cursor.execute("INSERT INTO azurcam.users(name, username, email, password)")
+
+
+pnconfig = PNConfiguration()
+pnconfig.subscribe_key = "sub-c-60d39bd0-5cfb-11ec-96e9-32997ff5e1b9"
+pnconfig.publish_key = "pub-c-0586af82-d21f-4eb0-a261-9b0ec1e03ba0"
+
+pubnub = PubNub(pnconfig)
+
+pubnub.add_listener(MySubscribeCallback())
+
+pubnub.subscribe().channels("azurcam-channel").with_presence() \
+    .execute()
 
 if __name__ == '__main__':
     app.run()
